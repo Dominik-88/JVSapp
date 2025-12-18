a57d59b1b5d81beb003262d7be351c676dfe374e
free_Domek <dominikschmied4@gmail.com>
2025-12-18 02:59:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,11 @@
-const CACHE_NAME = "jvs-cache-v1";
-const ASSETS = [
-  "/", "/index.html", "/manifest.json",
-  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
-  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-];
-
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+const CACHE="jvs-pwa-v1";
+const ASSETS=["/","/index.html","/manifest.json","/arealy.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const clone=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,clone)); return res;
+  })));
 });
-
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-      return caches.open(CACHE_NAME).then(c=>{c.put(e.request,res.clone());return res;});
-    }).catch(()=>caches.match("/index.html")))
-  );
-});
-
-self.addEventListener("activate", e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
-  );
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
 });
